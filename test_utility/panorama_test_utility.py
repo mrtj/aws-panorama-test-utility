60f9282067ccda632f5e0caf2046e4309316bfd0
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2023-02-15 11:03:48 +0100

    Update from original repo

--- test_utility/panorama_test_utility.py
+++ test_utility/panorama_test_utility.py
@@ -18,13 +18,13 @@ panorama_client = boto3.client('panorama') # FIXME : pass from sample notebook
 # ---
 
 class Config:
-    
+
     def __init__( self, **args ):
-        
+
         # By default, render output image on Jupyter notebook. This can be turned off in non-Jupyter environments.
         self.render_output_image_with_pyplot = True
         self.screenshot_dir = None
-        
+
         self.video_range = range(0,30,1)
 
         # FIXME : Should set default values for other parameters as well
@@ -42,7 +42,7 @@ class Config:
 
         os_name = platform.system()
         processor = platform.processor()
-        
+
         if os_name == "Linux":
             if processor == "aarch64":
                 self.compiled_model_suffix = "LINUX_ARM64"
@@ -64,10 +64,10 @@ _c = Config()
 # ---
 
 def configure( config ):
-    
+
     global _c
     _c = config
-        
+
     panoramasdk._configure(config)
 
 
@@ -81,7 +81,7 @@ class ProgressDots:
             if self.previous_status : print("")
             if status : print( status + " " , end="", flush=True)
             self.previous_status = status
-            
+
 
 def preview_text_file( filename ):
     print( filename + ":" )
@@ -105,7 +105,26 @@ def split_s3_path( s3_path ):
 
 def extract_targz( targz_filename, dst_dirname ):
     with tarfile.open( targz_filename, "r" ) as tar_fd:
-        tar_fd.extractall( path = dst_dirname )
+        def is_within_directory(directory, target):
+
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+
+            return prefix == abs_directory
+
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+
+            tar.extractall(path, members, numeric_owner=numeric_owner)
+
+
+        safe_extract(tar_fd, path=dst_dirname)
 
 
 def resolve_sm_role():
@@ -215,42 +234,42 @@ def download_sample_model( model_name, dst_dirname ):
     _http_download( src_url, dst_path )
 
     print( "Downloaded to", dst_path )
-    
-    
+
+
 def download_artifacts_gpu_sample(sample, account_id):
     if sample.upper() == 'ONNX':
         print('Downloading Source Code')
         os.system("wget -P ./onnx_37_app/packages/{}-onnx_37_app-1.0/ https://panorama-starter-kit.s3.amazonaws.com/public/v2/opengpusamples/ONNX_Sample/src.zip".format(str(account_id)))
         os.system("unzip -o ./onnx_37_app/packages/{}-onnx_37_app-1.0/src.zip -d ./onnx_37_app/packages/{}-onnx_37_app-1.0".format(str(account_id), str(account_id)))
         os.system("rm ./onnx_37_app/packages/{}-onnx_37_app-1.0/src.zip".format(str(account_id)))
-        
+
         print('Downloading Dependencies')
         os.system("wget -P . https://panorama-starter-kit.s3.amazonaws.com/public/v2/opengpusamples/ONNX_Sample/dependencies.zip")
         os.system("unzip -o dependencies.zip -d . ")
         os.system("rm dependencies.zip")
-        
+
     elif sample.upper() == 'PYTORCH':
         print('Downloading Source Code')
         os.system("wget -P ./yolov5s_37_2_app/packages/{}-yolov5s_37_2_app-1.0/ https://panorama-starter-kit.s3.amazonaws.com/public/v2/opengpusamples/PT_Sample/src.zip".format(str(account_id)))
         os.system("unzip -o ./yolov5s_37_2_app/packages/{}-yolov5s_37_2_app-1.0/src.zip -d ./yolov5s_37_2_app/packages/{}-yolov5s_37_2_app-1.0".format(str(account_id), str(account_id)))
         os.system("rm ./yolov5s_37_2_app/packages/{}-yolov5s_37_2_app-1.0/src.zip".format(str(account_id)))
-        
+
         print('Downloading Dependencies')
         os.system("wget -P . https://panorama-starter-kit.s3.amazonaws.com/public/v2/opengpusamples/PT_Sample/dependencies.zip")
         os.system("unzip -o dependencies.zip -d . ")
         os.system("rm dependencies.zip")
-        
+
     elif sample.upper() == 'TENSORFLOW':
         print('Downloading Source Code')
         os.system("wget -P ./tf2_4_trt_app/packages/{}-tf2_4_trt_app-1.0/ https://panorama-starter-kit.s3.amazonaws.com/public/v2/opengpusamples/TF_Sample/src.zip".format(str(account_id)))
         os.system("unzip -o ./tf2_4_trt_app/packages/{}-tf2_4_trt_app-1.0/src.zip -d ./tf2_4_trt_app/packages/{}-tf2_4_trt_app-1.0".format(str(account_id), str(account_id)))
         os.system("rm ./tf2_4_trt_app/packages/{}-tf2_4_trt_app-1.0/src.zip".format(str(account_id)))
-        
+
         print('Downloading Model')
         os.system("wget -P ./tf2_4_trt_app/packages/{}-tf2_4_trt_app-1.0/ https://panorama-starter-kit.s3.amazonaws.com/public/v2/opengpusamples/TF_Sample/saved_model_trt_fp16.zip".format(str(account_id)))
         os.system("unzip -o ./tf2_4_trt_app/packages/{}-tf2_4_trt_app-1.0/saved_model_trt_fp16.zip -d ./tf2_4_trt_app/packages/{}-tf2_4_trt_app-1.0".format(str(account_id), str(account_id)))
         os.system("rm ./tf2_4_trt_app/packages/{}-tf2_4_trt_app-1.0/saved_model_trt_fp16.zip".format(str(account_id)))
-        
+
         print('Downloading Dependencies')
         os.system("wget -P . https://panorama-starter-kit.s3.amazonaws.com/public/v2/opengpusamples/TF_Sample/dependencies.zip")
         os.system("unzip -o dependencies.zip -d . ")
@@ -261,13 +280,13 @@ def download_artifacts_gpu_sample(sample, account_id):
         os.system("wget -P ./trtpt_36_2_app/packages/{}-trtpt_36_2_app-1.0/ https://panorama-starter-kit.s3.amazonaws.com/public/v2/opengpusamples/TRT_Sample/src.zip".format(str(account_id)))
         os.system("unzip -o ./trtpt_36_2_app/packages/{}-trtpt_36_2_app-1.0/src.zip -d ./trtpt_36_2_app/packages/{}-trtpt_36_2_app-1.0".format(str(account_id), str(account_id)))
         os.system("rm ./trtpt_36_2_app/packages/{}-trtpt_36_2_app-1.0/src.zip".format(str(account_id)))
-        
+
         print('Downloading Dependencies')
         os.system("wget -P . https://panorama-starter-kit.s3.amazonaws.com/public/v2/opengpusamples/TRT_Sample/dependencies.zip")
         os.system("unzip -o dependencies.zip -d . ")
         os.system("rm dependencies.zip")
-        
-        
+
+
 
 
 def compile_model(
@@ -302,12 +321,12 @@ def compile_model(
             'MaxRuntimeInSeconds': 7200
         }
     }
-    
+
     if target_device:
         params["OutputConfig"]["TargetDevice"] = target_device
     if target_platform:
         params["OutputConfig"]["TargetPlatform"] = target_platform
-    
+
     response = sagemaker_client.create_compilation_job( **params )
 
     print( f'Created compilation job [{compilation_job_name}]' )
@@ -358,7 +377,7 @@ def prepare_model_for_test(
     compiled_model_filename = f"{model_name}-{_c.compiled_model_suffix}.tar.gz"
 
     # ---
-    
+
     print( f"Uploading [{model_filename}] to [{s3_model_location}] ..." )
 
     s3_client.upload_file(
@@ -397,7 +416,7 @@ def prepare_model_for_test(
     print( f"Extracting compiled model in [{output_model_dir}/{model_name}-{_c.compiled_model_suffix}] ..." )
 
     extract_targz(
-        f"{output_model_dir}/{compiled_model_filename}", 
+        f"{output_model_dir}/{compiled_model_filename}",
         f"{output_model_dir}/{model_name}-{_c.compiled_model_suffix}"
     )
 
@@ -419,33 +438,33 @@ def create_app(name, description, manifest, role, device):
 
 
 def list_app_instances( device_id = None ):
-    
+
     apps = []
-    
+
     next_token = None
-    
+
     while True:
-        
+
         params = {
             "MaxResults" : 25,
         }
 
         if device_id:
             params["DeviceId"] = device_id
-        
+
         if next_token:
             params["NextToken"] = next_token
-    
+
         response = panorama_client.list_application_instances( **params )
-        
+
         apps += response["ApplicationInstances"]
 
         if "NextToken" in response:
             next_token = response["NextToken"]
             continue
-        
+
         break
-    
+
     return apps
 
 
@@ -465,7 +484,7 @@ def deploy_app(device_id, app_name, role):
         device_id)
     assert app_created['ResponseMetadata']['HTTPStatusCode'] == 200
     app_id = app_created['ApplicationInstanceId']
-    
+
     print( "Application Instance Id :", app_id )
 
     progress_dots = ProgressDots()
@@ -476,7 +495,7 @@ def deploy_app(device_id, app_name, role):
         if status in ['DEPLOYMENT_SUCCEEDED','DEPLOYMENT_FAILED']:
             break
         time.sleep(60)
-    
+
     return response
 
 
@@ -517,4 +536,3 @@ def get_logs_url( region_name, device_id, application_instance_id ):
     log_group = f"/aws/panorama/devices/{device_id}/applications/{application_instance_id}"
     encoded_log_group = log_group.replace( "/", "$252F" )
     return f"https://console.aws.amazon.com/cloudwatch/home?region={region_name}#logsV2:log-groups/log-group/{encoded_log_group}"
-
